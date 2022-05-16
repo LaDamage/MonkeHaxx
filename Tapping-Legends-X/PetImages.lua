dfcd1ff269863072dfdb9ad8958c430b3999cff3
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2022-05-16 11:40:40 +0200

    Create PetImages.lua

---
+++ Tapping-Legends-X/PetImages.lua
@@ -0,0 +1,5 @@
+return {
+    ["10M Vexed Spirit"] = "https://cdn.discordapp.com/attachments/975685218411372585/975687426397831178/10M_Vexed_Spirit.png",
+    ["10M Shattered King"] = "https://cdn.discordapp.com/attachments/975685218411372585/975687025221054524/10M_Shattered_King.png",
+    ["10M Diamond King"] = "https://cdn.discordapp.com/attachments/975685218411372585/975686987317149746/10M_Diamond_King.png"
+}
