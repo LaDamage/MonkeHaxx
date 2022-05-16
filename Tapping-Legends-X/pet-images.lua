e93e3833226e8d2c60c69ceffc3c9f9537fd31f1
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2022-05-16 12:02:35 +0200

    Update pet-images.lua

--- Tapping-Legends-X/pet-images.lua
+++ Tapping-Legends-X/pet-images.lua
@@ -1,4 +1,24 @@
 return {
+    --// Lava Egg
+    ["Fiery Guardian"] = "https://cdn.discordapp.com/attachments/975685218411372585/975698625915142194/Fiery_Guardian.png",
+
+    --// Aqua Egg
+    ["Aqua Monster"] = "https://cdn.discordapp.com/attachments/975685218411372585/975698595422568468/Aqua_Monster.png",
+
+    --// Sakura Egg
+    ["Sakura Dragon"] = "https://cdn.discordapp.com/attachments/975685218411372585/975698570596462662/Sakura_Dragon.png",
+
+    --// Miner Egg
+    ["Miner Mole"] = "https://cdn.discordapp.com/attachments/975685218411372585/975698541869678592/Miner_Mole.png",
+
+    --// Galaxy Egg
+    ["Galactical Secrets"] = "https://cdn.discordapp.com/attachments/975685218411372585/975696210683895848/Galactical_Secrets.png",
+    ["Starlight Orbital"] = "https://cdn.discordapp.com/attachments/975685218411372585/975696248491372574/Starlight_Orbital.png",
+
+    --// 1M Egg
+    ["1M Easter Trophy"] = "https://cdn.discordapp.com/attachments/975685218411372585/975698520562606100/1M_Easter_Trophy.png",
+
+    --// 10M Egg
     ["10M Vexed Spirit"] = "https://cdn.discordapp.com/attachments/975685218411372585/975687426397831178/10M_Vexed_Spirit.png",
     ["10M Shattered King"] = "https://cdn.discordapp.com/attachments/975685218411372585/975687025221054524/10M_Shattered_King.png",
     ["10M Diamond King"] = "https://cdn.discordapp.com/attachments/975685218411372585/975686987317149746/10M_Diamond_King.png"
